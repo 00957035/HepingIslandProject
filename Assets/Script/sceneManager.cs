d9ddd7e32a180c10b96ea03aefe773acfa388d2d
Yu-Chien <00957035@email.ntou.edu.tw>
2023-02-17 21:42:58 +0800

    first upload

---
+++ Assets/Script/sceneManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+    public GameObject ChangeCanva;
+    public void change(){ //進入兌換場警
+        SceneManager.LoadScene(ChangeCanva.GetComponent<changeInfo>().SceneName);
+        //SceneManager.LoadScene(collision.gameObject.GetComponent<sceneInfo>().SceneName);
+    }
+    public void cancel(){ //關閉canvas
+       ChangeCanva.SetActive(false);
+    }
+}
