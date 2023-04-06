ee62fef41e1fb97473bc47b31de1c50cd114993c
cappuccino891021 <116076850+cappuccino891021@users.noreply.github.com>
2023-04-06 16:38:14 +0800

    shoppingScene

---
+++ Assets/Script/shoppingScene_script/ShowInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowInfo : MonoBehaviour
+{
+    Canvas InfoCanvas;
+    public Camera cam;
+    RaycastHit hitObj;
+    void Start()
+    {
+        InfoCanvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+        InfoCanvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = Input.mousePosition;
+        Ray mouseRay = cam.ScreenPointToRay(pos);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(mouseRay, out hitObj))
+            {
+                print(hitObj.collider.gameObject.tag);
+                if (hitObj.collider.gameObject.tag == "car")
+                    showInfoCanvas();
+            }
+        }
+    }
+
+    public void showInfoCanvas()
+    {
+        InfoCanvas.enabled = true;
+    }
+    public void hideInfoCanvas()
+    {
+        InfoCanvas.enabled = false;
+    }
+}
