07c22a1a80e5b4b8ca3521328d09e8930ec599d7
chengen <chengenl20@gmail.com>
2023-05-04 22:17:25 +0800

    UI

---
+++ Assets/MuseumAsset/Scripts/inputController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inputController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public InputField inputField;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnEndEdit(string str){
+        Debug.Log(str);
+    }
+}
