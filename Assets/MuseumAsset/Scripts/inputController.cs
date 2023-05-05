8b98c76b9de41c201306e942f237b9aea211084c
chengen <chengenl20@gmail.com>
2023-05-05 13:57:45 +0800

    UI

--- Assets/MuseumAsset/Scripts/inputController.cs
+++ Assets/MuseumAsset/Scripts/inputController.cs
@@ -7,6 +7,9 @@ public class inputController : MonoBehaviour
 {
     // Start is called before the first frame update
     public InputField inputField;
+    public GameObject museum;
+    Transform searchTransform;
+    GameObject targetPoint;
     void Start()
     {
         
@@ -19,6 +22,9 @@ public class inputController : MonoBehaviour
     }
 
     public void OnEndEdit(string str){
-        Debug.Log(str);
+        searchTransform = museum.transform.Find(str);
+        targetPoint = museum.transform.Find("targetPoint").gameObject;
+        targetPoint.SetActive(true);
+        targetPoint.transform.position = new Vector3(searchTransform.position.x, searchTransform.position.y + 5, searchTransform.position.z + 4);
     }
 }
