59d3322c9dd03f52031db1cd1a9fe05154d41bfd
chengen <chengenl20@gmail.com>
2023-05-18 23:40:40 +0800

    time

---
+++ Assets/MuseumAsset/Scripts/time.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class time : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        System.DateTime currentTime = System.DateTime.Now;
+        int hour = currentTime.Hour;
+        int min = currentTime.Minute;
+        int sec = currentTime.Second;
+        text.text = hour + " : " + min + " : " + sec;
+    }
+}
