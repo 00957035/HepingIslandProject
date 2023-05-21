94eaac1ca592a04f98c33c335850843060c77346
chengen <chengenl20@gmail.com>
2023-05-21 23:07:34 +0800

    g

---
+++ Assets/MuseumAsset/Scripts/dropdownController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dropdownController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Dropdown dropdown;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnValueChanged(int num){
+        Debug.Log(dropdown.options[num].text);
+    }
+
+}
