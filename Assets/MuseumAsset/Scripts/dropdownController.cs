7ca8d7c63dadb3ca5c88bceb8e3a80d1da9f0496
chengen <chengenl20@gmail.com>
2023-05-22 15:36:32 +0800

    g

--- Assets/MuseumAsset/Scripts/dropdownController.cs
+++ Assets/MuseumAsset/Scripts/dropdownController.cs
@@ -7,6 +7,9 @@ public class dropdownController : MonoBehaviour
 {
     // Start is called before the first frame update
     public Dropdown dropdown;
+    public GameObject museum;
+    public GameObject spotlight;
+    List<GameObject> flag = new List<GameObject>();
     void Start()
     {
         
@@ -19,7 +22,19 @@ public class dropdownController : MonoBehaviour
     }
 
     public void OnValueChanged(int num){
-        Debug.Log(dropdown.options[num].text);
+        if(flag != null){
+            foreach(GameObject prefab in flag){
+                Destroy(prefab);
+            }
+        }
+        foreach(Transform child in museum.transform){
+            if(child.gameObject.tag == dropdown.options[num].text){
+                spotlight.SetActive(true);
+                GameObject insSpotlight = Instantiate(spotlight);
+                insSpotlight.transform.position = new Vector3(child.position.x, child.position.y + 5, child.position.z);
+                flag.Add(insSpotlight);
+            }
+        }
     }
 
 }
