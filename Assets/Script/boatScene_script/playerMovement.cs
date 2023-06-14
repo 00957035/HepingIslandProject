bcfd077fc870d34306ae216080c4a0ddb3a18972
donutwu <terry910110@gmail.com>
2023-06-14 10:11:43 +0800

    naidemo

--- Assets/Script/boatScene_script/playerMovement.cs
+++ Assets/Script/boatScene_script/playerMovement.cs
@@ -13,55 +13,97 @@ public class playerMovement : MonoBehaviour
     private float speedDown = 4f; //減速加速度
     private float speedTend = 5f; //無操作時 速度超過0的加速度
     private float speedBack = 1f; //後退加速度
-
+    public ParticleSystem splash;
+    public ParticleSystem wave;
     private Transform m_Transform;
     public GameObject paddle;
     private Animation m_animation;
-
-    void Start(){
+    AudioSource audiosource;
+    public AudioClip wat;
+    //public GameObject splash;
+    void Start()
+    {
         m_Transform = this.transform;
         m_animation = paddle.GetComponent<Animation>();
+        audiosource = GetComponent<AudioSource>();
+        //splash = splash.GetComponent<ParticleSystem>();
     }
-    void Update() {
+    void Update()
+    {
         playerMove();
     }
-    public void OnCollisionEnter(Collision collision){ //collision為被碰之物件
-        //Debug.Log(collision.gameObject.name);
-        speedOne = speedOne * -1;
+    public void OnCollisionEnter(Collision collision)
+    { //collision為被碰之物件
+      //Debug.Log(collision.gameObject.name);
+        speedOne = speedOne * -1.0f;
     }
-    
-    void playerMove(){
-        if(Input.GetKey(KeyCode.Z)){
+
+
+    void playerMove()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
             speedOne = 0f;
         }
-        if(Input.GetKey(KeyCode.Q) && speedOne<speedMax){ //前進加速 Q左前 
+        if (Input.GetKey(KeyCode.Q) && speedOne < speedMax)
+        { //前進加速 Q左前 
             speedOne = speedOne + Time.deltaTime * speedUp; //加速
-            m_Transform.Rotate(0, 25f * Time.deltaTime, 0); //轉向
+            //m_Transform.Rotate(0, 25f * Time.deltaTime, 0); //轉向
             m_animation.Play("Rotate paddle");
-        }else if(Input.GetKey(KeyCode.W) && speedOne<speedMax){ //前進加速 W右前
+            audiosource.PlayOneShot(wat, 0.7f);
+            //gameObject.particleSystem.Play();
+            splash.Play();
+            Invoke(nameof(MethodName1), 0.35f);
+        }
+        else if (Input.GetKey(KeyCode.W) && speedOne < speedMax)
+        { //前進加速 W右前
             speedOne = speedOne + Time.deltaTime * speedUp; //加速
-            m_Transform.Rotate(0, -25f * Time.deltaTime, 0); //轉向
+            //m_Transform.Rotate(0, -25f * Time.deltaTime, 0); //轉向
             m_animation.Play("Rotate paddle2");
-        }else if(Input.GetKey(KeyCode.A) && speedOne>speedMin){ //向後減速 S右後
+            Invoke(nameof(MethodName2), 0.35f);
+        }
+        else if (Input.GetKey(KeyCode.A) && speedOne > speedMin)
+        { //向後減速 S右後
             // speedOne = Mathf.Lerp(speedOne, 0, 0.4f);
             speedOne = speedOne - Time.deltaTime * speedDown; //減速
-            m_Transform.Rotate(0, -25f * Time.deltaTime, 0); //轉向
+            //m_Transform.Rotate(0, -25f * Time.deltaTime, 0); //轉向
             m_animation.Play("Rotate paddle3");
-        }else if(Input.GetKey(KeyCode.S) && speedOne>speedMin){ //向後減速 S右後
+            Invoke(nameof(MethodName2), 0.35f);
+        }
+        else if (Input.GetKey(KeyCode.S) && speedOne > speedMin)
+        { //向後減速 S右後
             // speedOne = Mathf.Lerp(speedOne, 0, 0.4f); 
             speedOne = speedOne - Time.deltaTime * speedDown; //減速
-            m_Transform.Rotate(0, 25f * Time.deltaTime, 0); //轉向
+            //m_Transform.Rotate(0, 25f * Time.deltaTime, 0); //轉向
             m_animation.Play("Rotate paddle4");
+            splash.Play();
+            wave.Play();
+            Invoke(nameof(MethodName1), 0.35f);
         }
         //玩家未操作時 速度慢慢回到0
-        else if(!Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && speedOne>0f){ 
+        else if (!Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && speedOne > 0f)
+        {
             speedOne = speedOne - Time.deltaTime * speedTend;
-        }else if(!Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && speedOne<0f){ 
+        }
+        else if (!Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && speedOne < 0f)
+        {
             speedOne = speedOne + Time.deltaTime * speedTend;
         }
         Debug.Log(speedOne); //印出速度
         m_Transform.Translate(Vector3.forward * speedOne * Time.deltaTime); //移動
     }
+    void MethodName1()
+    {
+        m_Transform.Translate(Vector3.forward * speedOne * Time.deltaTime); //移動
+        m_Transform.Rotate(0, 25f * Time.deltaTime, 0);
+    }
+    void MethodName2()
+    {
+        m_Transform.Translate(Vector3.forward * speedOne * Time.deltaTime); //移動
+        m_Transform.Rotate(0, -25f * Time.deltaTime, 0);
+    }
+
+
     /*void playerMove(){
         if(Input.GetKey(KeyCode.Q)){ //左前
             Debug.Log("Q");
