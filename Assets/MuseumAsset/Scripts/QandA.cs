5017c95a59e515563c9a8dd275c212378adc70be
chengen <chengenl20@gmail.com>
2023-07-11 17:50:31 +0800

    g

---
+++ Assets/MuseumAsset/Scripts/QandA.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QandA : MonoBehaviour
+{
+    // Start is called before the first frame update
+    string [] plants = new string [] {"麒麟花", "黃紋龍舌蘭", "蒲葵", "草海桐", "文殊蘭", "厚葉石斑木", "銀葉鈕扣樹",
+    "蘄艾", "木麻黃", "大葉山欖", "欖仁樹", "白水木", "中東海棗", "臺灣海棗", "苧麻", "濱槐", "林投", "雙花蟛蜞菊",
+    "海桐", "射干", "蜘蛛百合", "臺灣蘆竹"};
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
