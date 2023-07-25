981cbfec26d86511fbd4caff790d4e86ae177131
chengen <chengenl20@gmail.com>
2023-07-25 21:21:45 +0800

    g

---
+++ Assets/MuseumAsset/Scripts/lightController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject directonal_Light;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onValueChanger(){
+        
+    }
+}
