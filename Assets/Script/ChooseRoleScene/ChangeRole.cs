2e27ce4982e81d070cf41206c637669545389060
cappuccino891021 <116076850+cappuccino891021@users.noreply.github.com>
2023-08-29 23:22:48 +0800

    0829

--- Assets/Script/ChooseRoleScene/ChangeRole.cs
+++ Assets/Script/ChooseRoleScene/ChangeRole.cs
@@ -8,13 +8,16 @@ public class ChangeRole : MonoBehaviour
     public Animator animator;
     public GameObject[] Roles;
     public Avatar[] avatars;
-    private bool flag = true;
+    //private bool flag = true;
     private int currentRoleNumber = 0;
     private characterManager manager;
     void Start()
     {
         currentRoleNumber = manager.GetCurrentOption();
-        Debug.Log(currentRoleNumber);
+        Debug.Log(currentRoleNumber+"!!");
+        animator.avatar = avatars[currentRoleNumber];
+        Roles[currentRoleNumber].SetActive(true);
+        Debug.Log("set role already");
     }
 
     // Update is called once per frame
